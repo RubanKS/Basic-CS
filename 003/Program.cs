97d0e39a594e783b765586bfe6622d7b3a0203a0
Konstantin Ru <kosru86@gmail.com>
2022-07-11 22:05:27 +0300

    003

---
+++ 003/Program.cs
@@ -0,0 +1,22 @@
+﻿int a;
+string? a1;
+int b;
+string? b1;
+
+Console.Write("Введите первое число:");
+a1 = Console.ReadLine();
+a = Convert.ToInt32(a1);
+Console.Write("Введите второе число:");
+b1 = Console.ReadLine();
+b = Convert.ToInt32(b1);
+
+if (a > b)
+{
+    Console.Write("Первое число больше = ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write("Второе число больше = ");
+    Console.WriteLine(b);
+}
