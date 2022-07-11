dd79a70c1f99fc5d5b9f644be602729f9fe15bfe
Konstantin Ru <kosru86@gmail.com>
2022-07-11 22:08:30 +0300

    005

---
+++ 005/Program.cs
@@ -0,0 +1,37 @@
+﻿int a;
+string? a1;
+int b;
+string? b1;
+int c;
+string? c1;
+
+//a1 = Convert.ToInt32(a1);
+
+
+Console.Write("Введите первое число:");
+a1 = Console.ReadLine();
+a = Convert.ToInt32(a1);
+
+Console.Write("Введите второе число:");
+b1 = Console.ReadLine();
+b = Convert.ToInt32(b1);
+
+Console.Write("Введите третье число:");
+c1 = Console.ReadLine();
+c = Convert.ToInt32(c1);
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+else
+{
+    max = a;
+    }
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine(max);
