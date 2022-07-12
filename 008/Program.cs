6e5534012ec7d70e5e813cffdcd11c48d13b883b
Konstantin Ru <kosru86@gmail.com>
2022-07-12 17:35:38 +0300

    008

---
+++ 008/Program.cs
@@ -0,0 +1,14 @@
+﻿//Вывести на экран числа от -N до N
+
+int N;
+
+Console.Write("Введите число:");
+N = Convert.ToInt32(Console.ReadLine());
+
+int a = -N;
+
+while (a <= N)
+{
+    Console.Write($"{a} ");
+    a++;
+}
