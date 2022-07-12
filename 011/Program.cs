d77740b647ed46ced4579eee06e0d7376ee09bdd
Konstantin Ru <kosru86@gmail.com>
2022-07-12 20:26:03 +0300

    011

---
+++ 011/Program.cs
@@ -0,0 +1,16 @@
+﻿// Дано число больше 9. Вывести на экран  вторую цифру числа с конца
+
+int n;
+int second;
+
+Console.Write("Введите число:");
+n = Convert.ToInt32(Console.ReadLine());
+
+while (n > 99)
+{
+    n = n / 10;
+}
+
+second = n % 10;
+
+Console.WriteLine(second);
