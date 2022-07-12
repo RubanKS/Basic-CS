e93ec40c193b71793447e226ff07a5dd8ac5274d
Konstantin Ru <kosru86@gmail.com>
2022-07-12 20:26:29 +0300

    012

---
+++ 012/Program.cs
@@ -0,0 +1,18 @@
+﻿//Дано число из  диапозона  [10, 99]. Показать наибольшую цифру числа
+
+int n;
+int max;
+
+Console.Write("Введите число от 10 до 99:");
+n = Convert.ToInt32(Console.ReadLine());
+
+max = n % 10;
+
+n = n / 10;
+
+if (n > max)
+{
+    max = n;
+}
+
+Console.WriteLine(max);
