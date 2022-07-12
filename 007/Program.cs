c370e3d712ad905726adbdffaa4d976bfc751f82
Konstantin Ru <kosru86@gmail.com>
2022-07-12 16:28:54 +0300

    007

---
+++ 007/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить является ли число чётным.
+
+int a;
+Console.Write("Введите число:");
+a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 1)
+{
+    Console.WriteLine("Число нечётное");
+
+}
+else
+{
+    Console.WriteLine("Число чётное");
+}
