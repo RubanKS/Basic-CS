94c227456888360b26abb42227ade6c07ddb814c
Konstantin Ru <kosru86@gmail.com>
2022-07-13 23:23:52 +0300

    013

---
+++ 013/Program.cs
@@ -0,0 +1,13 @@
+﻿//Удалить вторую цифру целого числа введенного с клавиатуры.
+
+Console.Write("Введите число:");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+int last = a % 10;
+
+int a0 = a / 100;
+
+a = a0 *10 + last;
+
+Console.WriteLine(a);
