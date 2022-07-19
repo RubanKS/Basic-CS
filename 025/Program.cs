da16bf30bbe34c6477fcb16d5bd24397e5d05022
Konstantin Ru <kosru86@gmail.com>
2022-07-19 14:35:00 +0300

    025

---
+++ 025/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вывести на экран кубы чисел от 1 до N
+
+int N;
+
+Console.Write("Введите число:");
+N = Convert.ToInt32(Console.ReadLine());
+
+int a = 1;
+double cube = 1;
+
+while (a <= N)
+{
+    cube = a * a * a;
+    Console.Write($"{cube} ");
+    a++;
+}
+
