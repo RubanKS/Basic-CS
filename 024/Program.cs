c5c06dd9ea28fe92d801732e2c70dcddeacbb0d7
Konstantin Ru <kosru86@gmail.com>
2022-07-19 15:34:39 +0300

    024

---
+++ 024/Program.cs
@@ -0,0 +1,16 @@
+﻿// Вывести на экран таблицу квадратов чисел от 1 до N
+
+int N;
+
+Console.Write("Введите число:");
+N = Convert.ToInt32(Console.ReadLine());
+
+int a = 1;
+double square = 1;
+
+while (a <= N)
+{
+    square = a * a;
+    Console.Write($"{square} ");
+    a++;
+}
