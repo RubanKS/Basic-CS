238965ef9eaab06e2f4723ccb2f1ed9bcf961298
Konstantin Ru <kosru86@gmail.com>
2022-07-19 14:14:53 +0300

    022

---
+++ 022/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа проверяет пятизначное число на палиндромом
+
+Console.Write("Введите число:");
+string s = (Console.ReadLine());
+
+if (s.Reverse().SequenceEqual(s)) Console.WriteLine("Палиндром");
+else Console.WriteLine("Не Палиндром");
