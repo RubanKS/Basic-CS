779a36f52d7b86314c64f142449a5512abd26da7
Konstantin Ru <kosru86@gmail.com>
2022-07-19 13:09:53 +0300

    021

---
+++ 021/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.Write("Введите номер четверти:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n == 1) System.Console.WriteLine("Возможные координаты лежат x>0 и y>0");
+if (n == 2) System.Console.WriteLine("Возможные координаты лежат x<0 и y>0");
+if (n == 3) System.Console.WriteLine("Возможные координаты лежат x<0 и y<0");
+if (n == 4) System.Console.WriteLine("Возможные координаты лежат x>0 и y<0");
+
+
+/*
+int AreaN(double n)
+{
+    System.Console.WriteLine($"x={x} y={y}");
+    if (x>0 && y>0) return 1;
+    if (x<0 && y>0) return 2;
+    if (x>0 && y>0) return 3;
+    if (x>0 && y>0) return 4;
+    return 0;
+}
+*/
+
+/*
+int Quarter (int n)
+{
+    if (n == 1) return Convert.ToString("x>0 && y>0");
+    if (n == 2) return Convert.ToString("x<0 && y>0");
+    if (n == 3) return Convert.ToString("x<0 && y<0");
+    if (n == 4) return Convert.ToString("x>0 && y<0");
+    return 0;
+}
+*/
+
