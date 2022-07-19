d5e1d1bb0b4e1c6bd5e060ca2fbed60c819802a2
Konstantin Ru <kosru86@gmail.com>
2022-07-19 14:55:17 +0300

    029

---
+++ 029/Program.cs
@@ -0,0 +1,14 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.Write("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+
+while (n > 0)
+{
+    sum = sum + n % 10;
+    n = n / 10;
+}
+
+System.Console.WriteLine(sum);
