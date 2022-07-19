a4af0db95da1f531fa9be86160ccfbf68ddd157a
Konstantin Ru <kosru86@gmail.com>
2022-07-19 14:17:03 +0300

    023

---
+++ 023/Program.cs
@@ -0,0 +1,13 @@
+﻿//Найти расстояние между точками в пространстве 2D
+
+double Distance(double x1, double y1, double x2, double y2)
+{
+    // double d;
+    // d=Math.Sqrt(Math.Pow(x2-x1,2) + Math.Pow(y1-y2,2));
+    // return d;
+    return Math.Sqrt(Math.Pow(x2-x1,2) + Math.Pow(y1-y2,2));
+}
+
+double x1=0,y1=0,x2=10,y2=10;
+
+System.Console.WriteLine(Distance(x1,y1,x2,y2));
