b4a4527e1768d8aaaab7e8c04100b2a5abf2f12b
Konstantin Ru <kosru86@gmail.com>
2022-07-19 15:05:48 +0300

    030

---
+++ 030/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.Write("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int a = 1;
+double product = 1;
+
+while (a <= N)
+{
+    product = product * a;
+    a++;
+}
+
+Console.Write($"{product}");
