24801069ecbae29ac392636b592436175abb20cc
Konstantin Ru <kosru86@gmail.com>
2022-07-23 18:40:40 +0300

    038

---
+++ 038/Program.cs
@@ -0,0 +1,11 @@
+﻿//В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int count = 0;
+
+int[] a = new int[123];
+Random random = new Random();
+
+for (int i = 10; i < 99; i++)
+    count = count + 1;
+
+System.Console.WriteLine($"В отрезке всего элементов: {count}");
