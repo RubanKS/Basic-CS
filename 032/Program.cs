b90a0eb8c49387272d777d7cdfe2e5edff5fdb17
Konstantin Ru <kosru86@gmail.com>
2022-07-23 17:38:17 +0300

    032

---
+++ 032/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задать массив из 8 целых элементов и вывести их на экран
+
+int N = 8;
+
+int[] a = new int[N];// создание нового массива
+Random random = new Random();//псевдослучайные числа
+for (int i = 0; i < a.Length; i++)
+    a[i] = random.Next(0, 10);
+for (int i = 0; i < a.Length; i++)
+    System.Console.Write($"{a[i],4}");//4 - количество знаков для вывода
+System.Console.WriteLine();
