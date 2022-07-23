ea2baca2a5327a35d4212ea1f40b73a1e6e6813f
Konstantin Ru <kosru86@gmail.com>
2022-07-23 17:39:13 +0300

    035

---
+++ 035/Program.cs
@@ -0,0 +1,21 @@
+﻿//Написать программу замены элементов массива на противоположные
+
+Console.Write("Введите количество чисел:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[] a = new int[N];
+Random random = new Random();
+
+for (int i = 0; i < a.Length; i++)
+{
+    a[i] = random.Next(-9, 10);
+    System.Console.Write($"{a[i],5}");
+}
+
+System.Console.WriteLine();
+
+for (int i = 0; i < a.Length; i++)
+{    
+    a[i] = -a[i];
+    System.Console.Write($"{a[i],5}");
+}
