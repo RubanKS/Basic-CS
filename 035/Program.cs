f3a9fcbf7cdaa07aaeefd5c71769d2894d7de9ff
Konstantin Ru <kosru86@gmail.com>
2022-08-08 21:01:10 +0300

    upd 035

--- 035/Program.cs
+++ 035/Program.cs
@@ -1,21 +1,57 @@
 ﻿//Написать программу замены элементов массива на противоположные
 
-Console.Write("Введите количество чисел:");
-int N = Convert.ToInt32(Console.ReadLine());
-
+int N =0;
 int[] a = new int[N];
-Random random = new Random();
+Input(out N);
+Init (out a, min:-10, max:11);
+Print();
 
-for (int i = 0; i < a.Length; i++)
+
+
+
+
+/*---Input--------------------------------*/
+int Input(out int N)
 {
-    a[i] = random.Next(-9, 10);
-    System.Console.Write($"{a[i],5}");
+    Console.Write("Введите количество чисел:");
+    N = Convert.ToInt32(Console.ReadLine());
+    return N;
 }
 
-System.Console.WriteLine();
-
-for (int i = 0; i < a.Length; i++)
-{    
-    a[i] = -a[i];
-    System.Console.Write($"{a[i],5}");
+/*---Init--------------------------------*/
+void Init (out int[] a, int min = -100, int max = 100)
+{
+    a = new int[N];
+    Random random = new Random();//псевдослучайные числа
+    for (int i = 0; i < a.Length; i++)
+        a[i] = random.Next(min, max);
 }
+
+/*---Print-------------------------------*/
+void Print ()
+{
+    PrintA(a);
+    PrintB(a);
+}
+
+/*---PrintA-------------------------------*/
+void PrintA (int[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+    {
+        // a[i] = random.Next(-9, 10);
+        System.Console.Write($"{a[i],5}");
+    }
+    System.Console.WriteLine();
+}
+
+/*---PrintB-------------------------------*/
+void PrintB (int[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+    {    
+        a[i] = -a[i];
+        System.Console.Write($"{a[i],5}");
+    }
+}
+
