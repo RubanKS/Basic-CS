34065165b0adb638b87a977cedcb4982ebee22ac
Konstantin Ru <kosru86@gmail.com>
2022-08-08 21:01:58 +0300

    upd 037

--- 037/Program.cs
+++ 037/Program.cs
@@ -3,22 +3,45 @@
 
 int countEven = 0;
 int countOdd = 0;
+int N = 0;
 
-Console.Write("Введите количество чисел:");
-int N = Convert.ToInt32(Console.ReadLine());
+Input(out N);
 
 int[] a = new int[N];
 Random random = new Random();
 
-for (int i = 0; i < a.Length; i++)
+Count(countEven, countOdd);
+Print();
+
+
+
+
+/*---Input--------------------------------*/
+int Input(out int N)
 {
-    a[i] = random.Next(100, 1000);
-    //System.Console.Write($"{a[i],5}\n");
-    if (a[i] % 2 == 0)
-        countEven = countEven + 1;
-    else
-        countOdd = countOdd + 1;
+    Console.Write("Введите количество чисел:");
+    N = Convert.ToInt32(Console.ReadLine());
+    return N;
 }
 
-System.Console.WriteLine($"Всего чётных чисел: {countEven}");
-System.Console.WriteLine($"Всего нечётных чисел: {countOdd}");
+/*---Count--------------------------------*/
+int Count(int countE, int countO)
+{
+    for (int i = 0; i < a.Length; i++)
+    {
+        a[i] = random.Next(100, 1000);
+        // System.Console.Write($"{a[i],5}\n");
+        if (a[i] % 2 == 0)
+            countEven = countEven + 1;
+        else
+            countOdd = countOdd + 1;
+    }
+    return 0;
+}
+
+/*---Print-------------------------------*/
+void Print()
+{
+    System.Console.WriteLine($"Всего чётных чисел: {countEven}");
+    System.Console.WriteLine($"Всего нечётных чисел: {countOdd}");
+}
