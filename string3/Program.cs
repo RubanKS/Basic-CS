b4efb7a93dd5f9f342cdfbf6ba649263115ec32f
Konstantin Ru <kosru86@gmail.com>
2022-08-23 17:23:16 +0300

    string3

---
+++ string3/Program.cs
@@ -0,0 +1,27 @@
+﻿// С клавиатуры вводится строка разделенная точкой.
+//Подсчитать количество символов до точки
+
+string text;
+Input (out text);
+int before = text.IndexOf('.');
+Print(before);
+
+
+
+
+
+
+
+
+
+string Input(out string t)
+{
+Console.WriteLine("Введите текст:");
+t = Console.ReadLine();
+return t;
+}
+
+void Print(int before)
+{
+Console.WriteLine($"До точки {before} символов");
+}
